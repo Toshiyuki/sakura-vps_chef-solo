fb4b12b1db4ddb58915303265c5bfa8cf4a6450b
KentaKatoh <kenta@diverta.co.jp>
2013-04-19 12:18:44 +0900

    Postgresqlとか追加。

--- cookbooks/rcms/recipes/default.rb
+++ cookbooks/rcms/recipes/default.rb
@@ -2,6 +2,22 @@
 # php
 #
 
+package "epel" do
+    action :install
+    not_if "rpm -q epel-release"
+    source http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-7.noarch.rpm
+end
+package "remi" do
+    action :install
+    not_if "rpm -q remi-release"
+    source http://rpms.famillecollet.com/enterprise/remi-release-6.rpmm
+end
+package "nginx" do
+    action :install
+    not_if "rpm -q nginx"
+    source http://nginx.org/packages/centos/5/noarch/RPMS/nginx-release-centos-5-0.el5.ngx.noarch.rpm
+end
+
 #
 # Package install
 #
@@ -40,13 +56,25 @@
     xpdf
     s3cmd
     sysstat
-    vixie-cron
 }.each do |pkgname|
     package "#{pkgname}" do
         action :install
         not_if "rpm -q #{pkgname}"
     end
 end
+
+remote_file "/tmp/postgresql-9.2.4.tar.gz" do
+    source "ftp://ftp2.jp.postgresql.org/pub/postgresql/source/v9.2.4/postgresql-9.2.4.tar.gz"
+end
+
+execute "install-postgresql" do
+    command "tar zxvf postgresql-9.2.4.tar.gz -C /tmp/ && cd /tmp/postgresql-9.2.4 && ./configure && make && make install"
+end
+
+execute "setup-postgresql" do
+    command "cp /tmp/postgresql-9.2.4/contrib/start-scripts/linux /etc/init.d/postgresql && chmod 755 /etc/init.d/postgresql"
+end
+
 
 
 #
