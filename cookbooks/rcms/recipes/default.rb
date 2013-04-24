987a24a232a0130b2494963010987b270d076524
KentaKatoh <kenta@diverta.co.jp>
2013-04-24 23:58:33 +0900

    少し変更

--- cookbooks/rcms/recipes/default.rb
+++ cookbooks/rcms/recipes/default.rb
@@ -68,13 +68,22 @@ remote_file "/tmp/postgresql-9.2.4.tar.gz" do
 end
 
 execute "install-postgresql" do
-    command "tar zxvf /tmp/postgresql-9.2.4.tar.gz -C /tmp/ && cd /tmp/postgresql-9.2.4 && ./configure && make && make install"
+    command "cd /tmp/ && tar zxvf postgresql-9.2.4.tar.gz && cd postgresql-9.2.4 && ./configure && make && make install"
 end
 
 execute "setup-postgresql" do
     command "cp /tmp/postgresql-9.2.4/contrib/start-scripts/linux /etc/init.d/postgresql && chmod 755 /etc/init.d/postgresql"
 end
 
+git "/tmp/phantomjs" do
+    repository "git://github.com/ariya/phantomjs.git"
+    reference "1.9"
+    action :sync
+end
+
+execute "install-phantomjs" do
+    command "cd /tmp/ && bzip2 -dc phantomjs-1.9.0-linux-x86_64.tar.bz2 | tar zxvf -  && cd phantomjs-1.9.0-linux-x86_64 && ./build.sh --confirm"
+end
 
 
 #
